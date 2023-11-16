52322895b4bcb165f5dbd592cf082a565e4fd92d
NdaruWindra <ndaruw29@gmail.com>
2023-11-16 21:32:19 +0700

    Create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div></div>
+      <div className="w-screen flex flex-row justify-center bg-black text-white py-3 gap-4">
+        <a href="">Beranda</a>
+        <a href="">Edukasi</a>
+        <a href="">Toko</a>
+        <a href="">Komunitas</a>
+      </div>
     </>
   );
 };
