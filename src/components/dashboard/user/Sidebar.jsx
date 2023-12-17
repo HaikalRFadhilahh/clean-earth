fbd4ee1d78b938374510fb78999b903fb372ddd0
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2023-12-17 20:43:07 +0700

    Transaksi: Dynamic Data From API Done

--- src/components/dashboard/user/Sidebar.jsx
+++ src/components/dashboard/user/Sidebar.jsx
@@ -59,41 +59,41 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
           sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
         }`}
-        aria-hidden="true"
+        aria-hidden='true'
       ></div>
 
       {/* Sidebar */}
       <div
-        id="sidebar"
+        id='sidebar'
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 dark:bg-[#2d4e38] p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-[#2d4e38] p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? "translate-x-0" : "-translate-x-64"
         }`}
       >
         {/* Sidebar header */}
-        <div className="flex justify-between pr-3 sm:px-2">
+        <div className='flex justify-between pr-3 sm:px-2'>
           {/* Close button */}
           <button
             ref={trigger}
-            className="lg:hidden text-slate-500 hover:text-slate-400"
+            className='lg:hidden text-slate-500 hover:text-slate-400'
             onClick={() => setSidebarOpen(!sidebarOpen)}
-            aria-controls="sidebar"
+            aria-controls='sidebar'
             aria-expanded={sidebarOpen}
           >
-            <span className="sr-only">Close sidebar</span>
+            <span className='sr-only'>Close sidebar</span>
             <svg
-              className="w-6 h-6 fill-current"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+              className='w-6 h-6 fill-current'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
             >
-              <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
+              <path d='M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z' />
             </svg>
           </button>
         </div>
 
-        <NavLink to="/profile">
-          <div className="flex justify-center mt-5">
-            <img src={Logo} alt="" width="100" height="100" />
+        <NavLink to='/profile'>
+          <div className='flex justify-center mt-5'>
+            <img src={Logo} alt='' width='100' height='100' />
           </div>
           <h2
             className={
@@ -105,30 +105,30 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         </NavLink>
 
         {/* Links */}
-        <div className="space-y-8 mt-16">
+        <div className='space-y-8 mt-16'>
           <div>
-            <h3 className="text-2xl font-poppins text-white font-semibold text-center">
+            <h3 className='text-2xl font-poppins text-white font-semibold text-center'>
               <span
-                className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
-                aria-hidden="true"
+                className='hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6'
+                aria-hidden='true'
               >
                 •••
               </span>
-              <span className=" font-poppins lg:hidden lg:sidebar-expanded:block 2xl:block">
+              <span className=' font-poppins lg:hidden lg:sidebar-expanded:block 2xl:block'>
                 CleanEarth
               </span>
             </h3>
 
-            <div className="flex justify-center mt-5">
-              <ul className="flex flex-col justify-between h-full gap-5 mt-5 ">
+            <div className='flex justify-center mt-5'>
+              <ul className='flex flex-col justify-between h-full gap-5 mt-5 '>
                 <NavLink
                   className={`block text-slate-200 truncate transition duration-150`}
-                  to="/profile"
+                  to='/profile'
                 >
-                  <div className="flex items-center justify-between group">
-                    <div className="flex items-center ">
-                      <IoMdHome className="text-slate-200 w-7 h-7 group-hover:text-green-500 " />
-                      <span className=" font-poppins text-base font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500">
+                  <div className='flex items-center justify-between group'>
+                    <div className='flex items-center '>
+                      <IoMdHome className='text-slate-200 w-7 h-7 group-hover:text-green-500 ' />
+                      <span className=' font-poppins text-base font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500'>
                         Dashboard{" "}
                       </span>
                     </div>
@@ -137,12 +137,12 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
                 <NavLink
                   className={`block text-slate-200 truncate transition duration-150`}
-                  to="/profile/transaksi"
+                  to='/profile/transaksi'
                 >
-                  <div className="flex items-center justify-between group">
-                    <div className="flex items-center ">
-                      <GrTransaction className="text-slate-200 w-7 h-7 group-hover:text-green-500" />
-                      <span className=" font-poppins text-base font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500">
+                  <div className='flex items-center justify-between group'>
+                    <div className='flex items-center '>
+                      <GrTransaction className='text-slate-200 w-7 h-7 group-hover:text-green-500' />
+                      <span className=' font-poppins text-base font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500'>
                         Transaksi{" "}
                       </span>
                     </div>
@@ -151,12 +151,12 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
                 <NavLink
                   className={`block text-slate-200 truncate transition duration-150`}
-                  to="/profile/akunsaya"
+                  to='/profile/akunsaya'
                 >
-                  <div className="flex items-center justify-between group ">
-                    <div className="flex items-center ">
-                      <IoPersonCircleOutline  className="text-slate-200 w-7 h-7 group-hover:text-green-500" />
-                      <span className=" font-poppins text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500">
+                  <div className='flex items-center justify-between group '>
+                    <div className='flex items-center '>
+                      <IoPersonCircleOutline className='text-slate-200 w-7 h-7 group-hover:text-green-500' />
+                      <span className=' font-poppins text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500'>
                         Akun Saya{" "}
                       </span>
                     </div>
@@ -165,12 +165,12 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
                 <NavLink
                   className={`block text-slate-200 truncate transition duration-150`}
-                  to="/"
+                  to='/'
                 >
-                  <div className="flex items-center justify-between group mt-40">
-                    <div className="flex items-center">
-                      <RxExit  className="text-slate-200 w-7 h-7 group-hover:text-green-500" />
-                      <span className=" font-poppins text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500">
+                  <div className='flex items-center justify-between group mt-40'>
+                    <div className='flex items-center'>
+                      <RxExit className='text-slate-200 w-7 h-7 group-hover:text-green-500' />
+                      <span className=' font-poppins text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 group-hover:text-green-500'>
                         Keluar{" "}
                       </span>
                     </div>
@@ -182,19 +182,19 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         </div>
 
         {/* Expand / collapse button */}
-        <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
-          <div className="px-3 py-2">
+        <div className='pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto'>
+          <div className='px-3 py-2'>
             <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
-              <span className="sr-only">Expand / collapse sidebar</span>
+              <span className='sr-only'>Expand / collapse sidebar</span>
               <svg
-                className="w-6 h-6 fill-current sidebar-expanded:rotate-180"
-                viewBox="0 0 24 24"
+                className='w-6 h-6 fill-current sidebar-expanded:rotate-180'
+                viewBox='0 0 24 24'
               >
                 <path
-                  className="text-slate-400"
-                  d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"
+                  className='text-slate-400'
+                  d='M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z'
                 />
-                <path className="text-slate-600" d="M3 23H1V1h2z" />
+                <path className='text-slate-600' d='M3 23H1V1h2z' />
               </svg>
             </button>
           </div>
