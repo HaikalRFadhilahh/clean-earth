1261380f8413de2ce8befd3ed0f83739bc3a732b
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2023-12-17 17:33:55 +0700

    Edit User: Kurang Image and Password

--- src/pages/dashboard/user/AkunSaya.jsx
+++ src/pages/dashboard/user/AkunSaya.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ImgProfile from "../../../assets/img/Profile.png";
 import { datausers } from "../../../store";
 import { useRecoilState } from "recoil";
 import guestUsers from "../../../assets/img/guestUsers.webp";
 import { convertToInternationalFormat } from "../../../function/convertToInternationalNumber";
+import { NavLink } from "react-router-dom";
 
 const AkunSaya = () => {
   const [datauser, setDatauser] = useRecoilState(datausers);
@@ -19,27 +19,31 @@ const AkunSaya = () => {
           <div>
             <img
               src={
-                datauser == null
+                datauser.image == null
                   ? guestUsers
                   : `${import.meta.env.VITE_API_SERVICE}${datauser.image}`
               }
               alt=''
               className={
-                "w-1/2 md:w-2/3 aspect-square object-cover rounded-full mx-auto"
+                "w-1/2 md:w-2/3 max-w-md aspect-square object-cover rounded-full mx-auto"
               }
             />
           </div>
           <div className='text-center font-poppins mt-5 w-full'>
             <h2 className='font-bold  text-3xl mt-5'>{datauser.nama}</h2>
+            <p className='mt-5 text-xl font-semibold'>{datauser.alamat}</p>
             <p className='mt-5 text-lg'>{datauser.email}</p>
             <p className='mt-5 text-lg'>
               {convertToInternationalFormat(datauser.kontak)}
             </p>
           </div>
           <div className='mt-5 font-poppins'>
-            <button className='bg-[#B0D9B1] p-2 w-28 rounded-xl'>
-              <a href='/profile/pengaturanbiodata'>Edit</a>
-            </button>
+            <NavLink
+              to={"/profile/pengaturanbiodata"}
+              className='bg-[#B0D9B1] py-3 px-6 w-28 rounded-xl'
+            >
+              Edit
+            </NavLink>
           </div>
         </div>
       </main>
--- src/pages/dashboard/user/PengaturanBiodata.jsx
+++ src/pages/dashboard/user/PengaturanBiodata.jsx
@@ -5,13 +5,17 @@ import guestUsers from "../../../assets/img/guestUsers.webp";
 import { datausers, token } from "../../../store";
 import { useRecoilState } from "recoil";
 import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const PengaturanBiodata = () => {
   const [tokenJWT, setTokenJWT] = useRecoilState(token);
+  const [datauser, setDatauser] = useRecoilState(datausers);
   const [useradata, setUserdata] = useState({});
   const navigate = useNavigate();
+  const [imageURL, setImageURL] = useState(null);
+  const [dataform, setDataform] = useState(null);
   useEffect(() => {
-    // console.log(tokenJWT);
     const getDataUsers = async () => {
       try {
         const result = await axios.post(
@@ -26,6 +30,22 @@ const PengaturanBiodata = () => {
           }
         );
         setUserdata(result.data.data);
+        setDataform({
+          nama: result.data.data.nama,
+          username: result.data.data.username,
+          gender: result.data.data.gender,
+          usia: result.data.data.usia,
+          alamat: result.data.data.alamat,
+          email: result.data.data.email,
+          kontak: result.data.data.kontak,
+        });
+        if (result.data.data.image == null) {
+          setImageURL(guestUsers);
+        } else {
+          setImageURL(
+            `${import.meta.env.VITE_API_SERVICE}${result.data.data.image}`
+          );
+        }
       } catch (error) {
         console.log(error);
         setTokenJWT(undefined);
@@ -34,6 +54,66 @@ const PengaturanBiodata = () => {
     };
     getDataUsers();
   }, [navigate]);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const imageURL = URL.createObjectURL(file);
+      setImageURL(imageURL);
+    }
+  };
+
+  const editData = async (e) => {
+    e.preventDefault();
+    try {
+      const updateData = await axios.put(
+        `${import.meta.env.VITE_API_SERVICE}/users/update`,
+        dataform,
+        {
+          headers: {
+            Authorization: tokenJWT,
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+        }
+      );
+      const validateUsers = await axios.post(
+        `${import.meta.env.VITE_API_SERVICE}/users/validate`,
+        {},
+        {
+          headers: {
+            Authorization: updateData.data.token,
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+        }
+      );
+      setTokenJWT(updateData.data.token);
+      setDatauser(validateUsers.data.data);
+      withReactContent(Swal).fire({
+        icon: "success",
+        title: "Berhasil",
+        text: "Update Data Berhasil",
+      });
+      navigate("/profile/akunsaya");
+    } catch (error) {
+      if (error.response.status == 400) {
+        const err = error.response.data.message[0];
+        withReactContent(Swal).fire({
+          icon: "error",
+          title: "Validation Error",
+          text: err.message,
+        });
+      } else {
+        withReactContent(Swal).fire({
+          icon: "error",
+          title: "Validation Error",
+          text: error.response.data.message,
+        });
+      }
+    }
+  };
 
   return (
     <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
@@ -73,22 +153,26 @@ const PengaturanBiodata = () => {
         <div className='sm:flex justify-center flex-col lg:flex-row mt-10'>
           <div className='w-96 flex flex-col items-center p-5'>
             <img
-              src={
-                useradata.image == null
-                  ? guestUsers
-                  : `${import.meta.env.VITE_API_SERVICE}${useradata.image}`
-              }
+              src={imageURL}
               alt=''
-              className={
-                "w-4/5 object-cover bg-center aspect-square rounded-full"
-              }
+              className={`w-4/5 object-cover bg-center aspect-square rounded-full`}
             />
-            <button className='bg-[#343735] font-poppins text-white p-2 w-40 rounded-xl mt-5'>
-              <a href='' className='flex items-center font-poppins'>
-                <MdOutlineImage className='w-5 h-5 mr-2' />
-                UNGGAH FOTO
-              </a>
-            </button>
+            <div className='bg-[#343735] font-poppins text-white p-2 w-2/3 rounded-xl mt-5 overflow-hidden flex items-center justify-center gap-3'>
+              <MdOutlineImage size={30} />
+              <label
+                htmlFor='fileInput'
+                className='cursor-pointer font-medium text-slate-100 text-center hover:text-slate-500 focus:outline-none focus:underline transition duration-150 ease-in-out'
+              >
+                Choose a file
+                <input
+                  id='fileInput'
+                  type='file'
+                  className='sr-only'
+                  accept='image/*'
+                  onChange={handleImageChange}
+                />
+              </label>
+            </div>
           </div>
 
           <div className='sm:flex ml-10 p-4 md:flex-col w-5/6'>
@@ -96,70 +180,148 @@ const PengaturanBiodata = () => {
               <div className='font-poppins py-4 px-2 rounded-lg text-xl shadow-xl flex justify-between items-center'>
                 <h1 className='font-bold'>Informasi Data Diri</h1>
                 <div>
-                  <button className='bg-[#949494] p-2 hidden md:inline-block w-20 rounded-xl mr-2 text-sm'>
-                    <a href=''>Batal</a>
-                  </button>
-                  <button className='bg-[#154EF9] p-2 hidden md:inline-block w-20 rounded-xl text-sm'>
-                    <a href=''>Simpan</a>
-                  </button>
+                  <NavLink
+                    to={"/profile/akunsaya"}
+                    className='bg-[#949494] p-2 hidden md:inline-block w-20 rounded-xl mr-2 text-sm text-center'
+                  >
+                    Batal
+                  </NavLink>
                 </div>
               </div>
               <div>
-                <form action='' className='flex flex-col gap-3 mt-5'>
+                <form
+                  action=''
+                  method={"POST"}
+                  className='flex flex-col gap-3 mt-5'
+                  onSubmit={editData}
+                >
                   <div className='flex flex-col gap-2'>
                     <label htmlFor='nama' className='font-poppins'>
                       Nama Lengkap
                     </label>
                     <input
+                      required={true}
                       type='text'
                       className='rounded border-[#343735]'
                       defaultValue={useradata.nama}
+                      onChange={(e) => {
+                        setDataform({ ...dataform, nama: e.target.value });
+                      }}
                     />
                   </div>
                   <div className='flex flex-col gap-2'>
-                    <label htmlFor='nama' className='font-poppins'>
+                    <label htmlFor='jenisKelamin' className='font-poppins'>
                       Jenis Kelamin
                     </label>
-                    <input type='text' className='rounded border-[#343735]' />
+                    <select
+                      required={true}
+                      id='jenisKelamin'
+                      className='rounded border-[#343735]'
+                      onChange={(e) => {
+                        setDataform({ ...dataform, gender: e.target.value });
+                      }}
+                    >
+                      <option
+                        value=''
+                        selected={useradata.gender == null ? true : false}
+                      >
+                        Pilih Jelas Kelamin
+                      </option>
+                      <option
+                        value='L'
+                        selected={useradata.gender == "L" ? true : false}
+                      >
+                        Laki-Laki
+                      </option>
+                      <option
+                        value='P'
+                        selected={useradata.gender == "P" ? true : false}
+                      >
+                        Perempuan
+                      </option>
+                    </select>
                   </div>
                   <div className='flex flex-col gap-2'>
                     <label htmlFor='nama' className='font-poppins'>
                       Usia
                     </label>
-                    <input type='text' className='rounded border-[#343735]' />
-                  </div>
-                </form>
-              </div>
-            </div>
-
-            <div className='relative sm:p-6 rounded-sm overflow-hidden'>
-              <div className='font-poppins py-4 pl-1 rounded-lg text-xl shadow-xl flex justify-between items-center'>
-                <h1 className='font-bold'>Ubah Kontak</h1>
-              </div>
-              <div>
-                <form action='' className='flex flex-col gap-3 mt-5'>
-                  <div className='flex flex-col gap-2'>
-                    <label htmlFor='nama' className='font-poppins'>
-                      Alamat Email
-                    </label>
-                    <input type='text' className='rounded border-[#343735]' />
+                    <input
+                      pattern='[0-9]*'
+                      required={true}
+                      type='text'
+                      className='rounded border-[#343735]'
+                      defaultValue={useradata.usia}
+                      onChange={(e) => {
+                        setDataform({
+                          ...dataform,
+                          usia: parseInt(e.target.value),
+                        });
+                      }}
+                    />
                   </div>
                   <div className='flex flex-col gap-2'>
                     <label htmlFor='nama' className='font-poppins'>
-                      Nomor Kontak
+                      Alamat
                     </label>
-                    <input type='text' className='rounded border-[#343735]' />
+                    <input
+                      required={true}
+                      type='text'
+                      className='rounded border-[#343735]'
+                      defaultValue={useradata.alamat}
+                      onChange={(e) => {
+                        setDataform({ ...dataform, alamat: e.target.value });
+                      }}
+                    />
+                  </div>
+                  <div className='font-poppins py-4 pl-1 rounded-lg text-xl shadow-xl flex justify-between items-center'>
+                    <h1 className='font-bold'>Ubah Kontak</h1>
+                  </div>
+                  <div>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor='nama' className='font-poppins'>
+                        Alamat Email
+                      </label>
+                      <input
+                        required={true}
+                        type='text'
+                        className='rounded border-[#343735]'
+                        defaultValue={useradata.email}
+                        onChange={(e) => {
+                          setDataform({ ...dataform, email: e.target.value });
+                        }}
+                      />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor='nama' className='font-poppins'>
+                        Nomor Kontak
+                      </label>
+                      <input
+                        required={true}
+                        type='text'
+                        className='rounded border-[#343735]'
+                        defaultValue={useradata.kontak}
+                        onChange={(e) => {
+                          setDataform({ ...dataform, kontak: e.target.value });
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className={"flex gap-x-2"}>
+                    <button
+                      className='px-3 py-1 md:py-2 text-center text-white bg-[#3559E0] rounded-lg text-sm w-fit'
+                      type={"submit"}
+                    >
+                      Simpan
+                    </button>
+                    <NavLink
+                      to={"/profile/akunsaya"}
+                      className='bg-[#949494] p-2 w-20 md:hidden rounded-xl mr-2 text-sm text-center'
+                    >
+                      Batal
+                    </NavLink>
                   </div>
                 </form>
               </div>
-            </div>
-            <div className='mt-5'>
-              <button className='bg-[#949494] p-2 w-20 md:hidden rounded-xl mr-2 text-sm'>
-                <a href=''>Batal</a>
-              </button>
-              <button className='bg-[#154EF9] p-2 w-20 md:hidden rounded-xl text-sm'>
-                <a href=''>Simpan</a>
-              </button>
             </div>
           </div>
         </div>
