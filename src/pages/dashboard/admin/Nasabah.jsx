d838121526734c2bb92bb9e1d7b4361454277f42
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2023-12-18 21:17:40 +0700

    Dashboard Admin: Nasabah Get Data

--- src/pages/dashboard/admin/Nasabah.jsx
+++ src/pages/dashboard/admin/Nasabah.jsx
@@ -1,125 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import SearchDashboard from '../../../components/SearchDashboard';
-import Button from "../../../components/Button";
+import React, { useState, useEffect } from "react";
+import SearchDashboard from "../../../components/SearchDashboard";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { datausers, token } from "../../../store";
+import { useRecoilState } from "recoil";
+import axios from "axios";
 
 const Nasabah = () => {
   const navigate = useNavigate();
-  const [nasabahData, setNasabahData] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  const [datauser, setDatauser] = useRecoilState(datausers);
+  const [tokenJWT, setTokenJWT] = useRecoilState(token);
+  const [dataUsers, setDataUsers] = useState([]);
 
   useEffect(() => {
-    fetchNasabahData();
-  }, []);
+    const getDataUsers = async () => {
+      try {
+        const result = await axios.post(
+          `${import.meta.env.VITE_API_SERVICE}/users/getusers`,
+          {},
+          {
+            headers: {
+              Authorization: tokenJWT,
+              Accept: "application/json",
+              "Content-Type": "application/json;charset=UTF-8",
+            },
+          }
+        );
 
-  const fetchNasabahData = async () => {
-    try {
-      const response = await fetch('https://precious-battledress-ray.cyclic.app/nasabah');
-      const data = await response.json();
-      setNasabahData(data);
-      setSearchResults(data);
-    } catch (error) {
-      console.error('Error fetching nasabah data:', error);
-    }
-  };
-
-  const handleSearch = (searchTerm) => {
-    if (!searchTerm.trim()) {
-      setSearchResults(nasabahData);
-    } else {
-      const filteredResults = nasabahData.filter((nasabah) =>
-        nasabah.nama.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setSearchResults(filteredResults);
-    }
-  };
-
-  const handleEdit = (id) => {
-    navigate(`/dashboard/editnasabah/${id}`);
-  };  
-
-  const handleDelete = async (id) => {
-    try {
-      const response = await fetch(`https://precious-battledress-ray.cyclic.app/nasabah/${id}`, {
-        method: 'DELETE',
-      });
-
-      if (response.ok) {
-        const updatedNasabahData = nasabahData.filter((nasabah) => nasabah._id !== id);
-        setNasabahData(updatedNasabahData);
-        setSearchResults(updatedNasabahData);
-      } else {
-        console.error('Failed to delete Nasabah:', response.status);
+        setDataUsers(result.data.data);
+        console.log(result.data.data);
+      } catch (error) {
+        setDatauser({});
+        setTokenJWT(undefined);
+        navigate("/masuk");
       }
-    } catch (error) {
-      console.error('Error deleting Nasabah:', error);
-    }
-  };
-  
+    };
+    getDataUsers();
+  }, [navigate]);
+
   return (
-    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+    <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
       <main>
-        <div className="relative mx-4 sm:p-6 rounded-sm overflow-hidden">
-          <h1 className="font-poppins p-4 rounded-lg text-2xl md:text-3xl bg-[#718977] text-white shadow-xl font-bold capitalize">
-            Nasabah
+        <div className='relative mx-4 sm:p-6 rounded-sm overflow-hidden'>
+          <h1 className='font-poppins p-4 rounded-lg text-2xl md:text-3xl bg-[#718977] text-white shadow-xl font-bold capitalize'>
+            Anggota
           </h1>
         </div>
         <div className='w-full flex justify-between p-4 sm:px-10'>
-          <SearchDashboard onSearch={handleSearch} />
-          <NavLink
-            className='w-fit h-fit px-5 py-2 my-1 ml-2 rounded-md bg-[#B0D9B1] '
-            to='/dashboard/tambahnasabah'
-          >
-            Tambah
-          </NavLink>
+          <SearchDashboard />
         </div>
-        <div className="m-8 overflow-auto">
-          <h2 className="text-xl font-semibold border-b-2 px-2 py-4 bg-[#EFF3F0]">Daftar Nasabah</h2>
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-[#EFF3F0]">
+        <div className='px-8 py-6 w-fullmin-h h-fit overflow-auto'>
+          <h2 className='text-xl font-semibold border-b-2 px-2 py-4 bg-[#EFF3F0]'>
+            Daftar Nasabah
+          </h2>
+          <table className='min-w-full divide-y divide-gray-200'>
+            <thead className='bg-[#EFF3F0]'>
               <tr>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  scope='col'
+                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                >
                   Nomor
                 </th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  scope='col'
+                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                >
                   Nama
                 </th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  scope='col'
+                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                >
                   No Telepon
                 </th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  scope='col'
+                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                >
                   Alamat
                 </th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  scope='col'
+                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                >
                   Aksi
                 </th>
               </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {searchResults.map((nasabah, index) => (
-                <tr key={nasabah._id}>
-                  <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{nasabah.nama}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{nasabah.no_telpon}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{nasabah.alamat}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <Button
-                      className='mr-2 bg-[#FFF383] text-black py-2 px-4 rounded'
-                      onClick={() => handleEdit(nasabah._id)}
-                    >
-                      <FaEdit/>
-                    </Button>
-                    <Button
-                      className='bg-[#EE5252] text-black py-2 px-4 rounded'
-                      onClick={() => handleDelete(nasabah._id)}
-                    >
-                      <MdDelete/>
-                    </Button>
+            <tbody className='bg-white divide-y divide-gray-200'>
+              {dataUsers.length == 0 ? (
+                <tr>
+                  <td
+                    colSpan={"5"}
+                    className={
+                      "text-center px-6 py-4 whitespace-nowrap font-semibold text-lg font-poppins"
+                    }
+                  >
+                    Belum Ada Anggota
                   </td>
                 </tr>
-              ))}
+              ) : (
+                <></>
+              )}
+              {dataUsers.map((item, i) => {
+                return (
+                  <tr key={item.id}>
+                    <td className='px-6 py-4 whitespace-nowrap'>{i + 1}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.nama}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                      {item.kontak}
+                    </td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                      {item.alamat}
+                    </td>
+                    <td>Coming Soon</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
